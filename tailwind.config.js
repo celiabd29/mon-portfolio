50ba2feee9fdd7bd5e18fa5446720af5e8f3c559
celiabd29 <c_abbad@stu-digital-campus.fr>
2025-04-03 12:56:32 +0200

    project&skill

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    "md:hidden",
+    "md:block",
+    "hidden",
+    "block",
+    "sm:hidden",
+    "sm:block",
+  ],
   theme: {
     extend: {
       fontFamily: {
